2c3869472db27dba6e0edb92956cb22cc75e0ceb
Werlley Ponte <werlleyponte@gmail.com>
2025-05-10 11:43:12 -0300

    feat(infraConfig): create environment validations

---
+++ src/infra/config/environments.interface.ts
@@ -0,0 +1,12 @@
+import { NodeEnvEnum } from "./environment.enum";
+
+export interface IConfig {
+  NODE_ENV: NodeEnvEnum;
+  PORT: number;
+  BASE_URL_MOVIES_SERVICE: string;
+  ACCESS_KEY_MOVIES_SERVICE: string;
+  BASE_URL_AI_SERVICE: string;
+  BASE_URL_TRANSLATOR: string;
+  ACCESS_KEY_TRANSLATOR: string;
+  AI_SERVICE_KEY: string;
+}
