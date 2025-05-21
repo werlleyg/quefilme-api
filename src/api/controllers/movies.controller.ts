ef0c3b4f890fb2565aaa686f3a9fd8ca2cfd362b
Werlley Ponte <werlleyponte@gmail.com>
2025-05-20 22:26:22 -0300

    feat(apiControllers): create MoviesController

---
+++ src/api/controllers/movies.controller.ts
@@ -0,0 +1,58 @@
+import Zod from "zod";
+import { Request, Response } from "express";
+import { makeGetMovieUsecase } from "../../infra/factories/usecases/getMovie.factory";
+import { makeGetMoviesUsecase } from "../../infra/factories/usecases/getMovies.factory";
+import { makeGetMovieSuggestionUsecase } from "../../infra/factories/usecases/getMovieSuggestion.factory";
+
+export class MoviesController {
+  /**
+   * Get Movie
+   * @param {Request} request - The Express request object with imdbId in path params.
+   * @param {Response} response - The Express response object to send the Movie as JSON.
+   */
+  public async getMovie(request: Request, response: Response) {
+    const paramsSchema = Zod.object({
+      imdbId: Zod.string(),
+    }).strict();
+
+    const { imdbId } = paramsSchema.parse(request.params);
+
+    const res = await makeGetMovieUsecase().exec(imdbId);
+
+    return response.status(200).json(res.toJSON);
+  }
+
+  /**
+   * Get Movies
+   * @param {Request} request - The Express request object with title in query params.
+   * @param {Response} response - The Express response object to send the Movies list as JSON.
+   */
+  public async getMovies(request: Request, response: Response) {
+    const querySchema = Zod.object({
+      title: Zod.string(),
+    }).strict();
+
+    const { title } = querySchema.parse(request.query);
+
+    const res = await makeGetMoviesUsecase().exec(title);
+
+    return response.status(200).json(res.map((m) => m.toJSON));
+  }
+
+  /**
+   * Get Movie Suggestion
+   * @param {Request} request - The Express request object with movies titles list in body.
+   * @param {Response} response - The Express response object to send the Suggestion Movie as JSON.
+   */
+  public async getMovieSuggestion(request: Request, response: Response) {
+    const bodySchema = Zod.object({
+      titles: Zod.array(Zod.string()),
+    }).strict();
+
+    const { titles } = bodySchema.parse(request.body);
+
+    const res = await makeGetMovieSuggestionUsecase().exec(titles);
+
+    return response.status(200).json(res.toJSON);
+  }
+}
