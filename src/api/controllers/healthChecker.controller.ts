490727ee43b75b856a4ac45dbf5b0a7f7785e9b2
Werlley Ponte <werlleyponte@gmail.com>
2025-05-23 00:00:17 -0300

    feat(apiControllers): create HealthCheckerController

---
+++ src/api/controllers/healthChecker.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { makeHealthCheckerUsecase } from "../../infra/factories/usecases/healthChecker.factory";
+
+export class HealthCheckerController {
+  /**
+   * Check Health
+   * @param {Request} _ .
+   * @param {Response} response - The Express response object to send the health check.
+   */
+  public async healthChecker(_: Request, response: Response) {
+    const res = await makeHealthCheckerUsecase().exec();
+
+    return response.status(200).json(res);
+  }
+}
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { MoviesController } from "./controllers";
+import { HealthCheckerController, MoviesController } from "./controllers";
 
 const routes = Router();
 const moviesRoutes = Router();
 const moviesController = new MoviesController();
+const checkHealthController = new HealthCheckerController();
 
 moviesRoutes.get("/", moviesController.getMovies.bind(moviesController));
 moviesRoutes.get("/:imdbId", moviesController.getMovie.bind(moviesController));
@@ -13,5 +14,11 @@ moviesRoutes.post(
 );
 
 routes.use("/movies", moviesRoutes);
+
+// health check
+routes.get(
+  "/",
+  checkHealthController.healthChecker.bind(checkHealthController),
+);
 
 export { routes };
