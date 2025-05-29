06a72edc96636f0876821df7a9e37a4a5b3e0922
Werlley Ponte <werlleyponte@gmail.com>
2025-05-29 00:13:05 -0300

    feat(infra): create ControllerLoggerPresenter

---
+++ src/infra/presenters/controllerLogger.presenter.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import { AppConfig } from "../config";
+
+export namespace ControllerLoggerPresenter {
+  export type MetadataReturn = {
+    controller: string;
+    method: string;
+    path: string;
+    statusCode?: number;
+    executionTime?: number;
+    requestId: string;
+    timestamp: string;
+    event: string;
+    userId?: string;
+    userAgent?: string;
+    ip?: string;
+    requestBody?: any;
+    requestParams?: any;
+    requestQuery?: any;
+    error?: any;
+  };
+}
+
+export class ControllerLoggerPresenter {
+  static metadata(
+    args: any,
+    target: any,
+    propertyKey: string,
+    event: string,
+  ): ControllerLoggerPresenter.MetadataReturn {
+    const [req, _res] = args as [Request, Response];
+    let requestId = req.headers["x-request-id"] || crypto.randomUUID();
+    if (Array.isArray(requestId)) {
+      requestId = requestId.join(",");
+    }
+
+    const metadata: ControllerLoggerPresenter.MetadataReturn = {
+      controller: target.constructor.name,
+      method: propertyKey,
+      path: req.baseUrl,
+      requestId: requestId as string,
+      timestamp: new Date().toISOString(),
+      userId: req.headers["x-user-id"]?.toString(),
+      userAgent: req.headers["user-agent"],
+      ip: req.ip,
+      requestBody: req.body,
+      requestParams: req.params,
+      requestQuery: req.query,
+      event: event,
+    };
+
+    return metadata;
+  }
+}
