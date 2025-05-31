fbb039154488d41aed9903730a8e655fdfbb12f8
Werlley Ponte <werlleyponte@gmail.com>
2025-05-31 01:23:09 -0300

    tests(infra): update makeGetMoviesUsecase unit tests to include logger service

--- tests/src/infra/factories/usecases/getMovies.factory.test.ts
+++ tests/src/infra/factories/usecases/getMovies.factory.test.ts
@@ -3,6 +3,7 @@ import {
   TranslatorService,
 } from "../../../../../src/domain/services";
 import { GetMoviesUsecaseImpl } from "../../../../../src/domain/usecases";
+import { makeLoggerService } from "../../../../../src/infra/factories/services/logger.factory";
 import { makeMoviesService } from "../../../../../src/infra/factories/services/movies.factory";
 import { makeTranslatorService } from "../../../../../src/infra/factories/services/translator.factory";
 import { makeGetMoviesUsecase } from "../../../../../src/infra/factories/usecases/getMovies.factory";
@@ -18,6 +19,10 @@ jest.mock(
     makeTranslatorService: jest.fn(),
   }),
 );
+
+jest.mock("../../../../../src/infra/factories/services/logger.factory", () => ({
+  makeLoggerService: jest.fn(),
+}));
 
 describe("makeGetMoviesUsecase", () => {
   it("should return an instance of GetMoviesUsecaseImpl", () => {
@@ -48,8 +53,15 @@ describe("makeGetMoviesUsecase", () => {
     const mockTranslatorService: TranslatorService = {
       translator: jest.fn(),
     };
+    const mockLoggerService = {
+      warn: jest.fn(),
+      info: jest.fn(),
+      error: jest.fn(),
+      setLabels: jest.fn(),
+    };
     (makeMoviesService as jest.Mock).mockReturnValue(mockMoviesService);
     (makeTranslatorService as jest.Mock).mockReturnValue(mockTranslatorService);
+    (makeLoggerService as jest.Mock).mockReturnValue(mockLoggerService);
 
     // Act
     const getMoviesUsecase = makeGetMoviesUsecase();
@@ -58,7 +70,11 @@ describe("makeGetMoviesUsecase", () => {
     expect(makeMoviesService).toHaveBeenCalled();
     expect(makeTranslatorService).toHaveBeenCalled();
     expect(getMoviesUsecase).toEqual(
-      new GetMoviesUsecaseImpl(mockMoviesService, mockTranslatorService),
+      new GetMoviesUsecaseImpl(
+        mockMoviesService,
+        mockTranslatorService,
+        mockLoggerService,
+      ),
     );
   });
 });
