07a226eb8d7fe59cacb3854916462293c80990cc
Werlley Ponte <werlleyponte@gmail.com>
2025-05-31 01:20:02 -0300

    feat(getMovies): add loggers

--- src/infra/factories/usecases/getMovies.factory.ts
+++ src/infra/factories/usecases/getMovies.factory.ts
@@ -1,7 +1,12 @@
 import { GetMoviesUsecaseImpl } from "../../../domain/usecases";
 import { GetMoviesUsecase } from "../../../domain/usecases/interfaces";
+import { makeLoggerService } from "../services/logger.factory";
 import { makeMoviesService } from "../services/movies.factory";
 import { makeTranslatorService } from "../services/translator.factory";
 
 export const makeGetMoviesUsecase = (): GetMoviesUsecase =>
-  new GetMoviesUsecaseImpl(makeMoviesService(), makeTranslatorService());
+  new GetMoviesUsecaseImpl(
+    makeMoviesService(),
+    makeTranslatorService(),
+    makeLoggerService(),
+  );
